1146b0cbc585e716c64990067196fd864094fddb
lvzhe22 <lvzhe22@users.noreply.github.com>
2015-05-18 10:45:22 -0400

    Create meiju.js

---
+++ meiju.js
@@ -0,0 +1,17 @@
+var old = [
+"",
+""
+];
+var pool = [
+"House of Cards",
+"Homeland",
+"True Detective",
+"The Affair",
+"Fargo",
+"Girls",
+"Orphan Black",
+"The Walking Dead",
+"Breaking Bad",
+"12 Monkeys"
+];
+console.log(pool[Math.floor(Math.random() * pool.length)]);
